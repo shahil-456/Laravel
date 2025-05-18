266ab45fdc40f47706006b2ce990184fa63b06fc
shahil-456 <shahilshadh037@gmail.com>
2025-05-18 19:07:45 +0530

    Update 2025_05_09_132852_create_customers_table.php

--- database/migrations/2025_05_09_132852_create_customers_table.php
+++ database/migrations/2025_05_09_132852_create_customers_table.php
@@ -11,6 +11,7 @@ return new class extends Migration {
         Schema::create('product_categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 100);
+            $table->timestamps();
         });
 
 
